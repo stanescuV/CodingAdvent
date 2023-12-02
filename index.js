5edb952984014d21f25161afa20d3596d9e0798e
Victore <stanescuv19@gmail.com.com>
2023-12-02 15:07:03 +0100

    Advent of Code 2023 #1

---
+++ index.js
@@ -0,0 +1,51 @@
+const p = document.getElementById("p");
+
+let str = JSON.stringify(p.textContent);
+// console.log(str);
+
+const strCopy = str.split("\\n  ")
+
+// console.log(strCopy)
+
+// arrStrings -> string uri separate 
+const arrStrings = strCopy.map(part=> part.trim())
+console.log(arrStrings)
+
+const arrNumbers = ["0","1","2","3","4","5","6","7","8","9"];
+// ex string : "46threevqs8114"
+
+// findingNumberInstring (2string1) output: [21]
+const arrRezultatFinal = [];
+function findingNumberInString (string) {
+    let arrRezultat = [];
+    for (let i = 0; i<=string.length; i++){
+        if (arrNumbers.includes(string[i])) {
+            arrRezultat.push(string[i]);
+        }
+    }
+    
+    if (arrRezultat.length=== 1){
+        arrRezultatFinal.push(`${arrRezultat[0]}` + `${arrRezultat[0]}`)
+    }
+
+    if (arrRezultat.length>1){
+        arrRezultatFinal.push(`${arrRezultat[0]}` + `${arrRezultat[arrRezultat.length-1]}`)
+    }
+    
+}
+
+// findingNumberInString("34threevqs9");
+
+// repeating  the function findingNumberInString
+function addingNumbers(array){
+    for(let i = 0 ; i<=array.length-1 ; i++){
+        findingNumberInString(array[i]);
+    }
+
+}
+
+addingNumbers(arrStrings);
+console.log(arrRezultatFinal)
+
+let x = arrRezultatFinal.reduce((a,b)=> a*1+b*1git config --global user.name "Mona Lisa",0 );
+console.log(x)
