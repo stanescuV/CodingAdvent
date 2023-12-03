57186f2c0ef0109d4d0ad798679a644ff9b0774b
Victore <stanescuv19@gmail.com.com>
2023-12-03 10:14:28 +0100

    2Digits and 1Digit work, need to adjust them tho

--- 2/index.js
+++ 2/index.js
@@ -6,16 +6,41 @@ let p1 = document.getElementById("p");
 let p = p1.textContent.replace(/\s/g, "");
 const pSplit = p.split("Game")
 
-console.log(pSplit)
+// console.log(pSplit)
 
 // make it cleaner 
 //ex : "8:3green,10red,15blue;1green,9red;9blue,2green,12red"
 // goal : {GameId : 8 , red: [10,9,12], blue:[15,9]...}
 
 function alpha(string) {
+    let gameId = string[0];
     let arrLetter = [];
     for (let i = 0; i < string.length; i++) {
         if (!isNaN(string[i] * 1)) {
+       
+       
+            // IF 2 DIGIT NUMBER
+        if (!isNaN(string[i] * 1) && !isNaN(string[i + 1] * 1)) {
+            let nr = string[i] + string[i + 1];
+            for (let a = i + 2; a < i + 7 && a < string.length; a++) {
+                arrLetter.push(string[a]);
+                let word = arrLetter.join("");
+                if (colors.includes(word)) {
+                    console.log("Match:", word, nr);
+                    return;
+                }
+                if (colors.includes(word.slice(0, -1))) {
+                    console.log("Match without last character:", word, nr);
+                    return;
+                }
+                if (colors.includes(word.slice(0, -2))) {
+                    console.log("Match without last two characters:", word, nr);
+                    return;
+                }
+            }
+        }
+
+
             // If current character is a digit
 
             // IF 1 DIGIT NUMBER
@@ -25,31 +50,26 @@ function alpha(string) {
                 let word = arrLetter.join("");
                 if( colors.includes(word) /*green*/){
                     console.log(word)
+                    console.log(nr)
                     return
                 }
 
                 if( colors.includes(word.slice(0,-1)) /*blue*/){
                     console.log(word)
+                    console.log(nr)
                     return
                 }
 
                 if( colors.includes(word.slice(0,-2)) /*red*/){
                     console.log(word)
+                    console.log(nr)
                     return 
                 }
             }
 
-            // IF 2 DIGIT NUMBER
-            // if (!isNaN(string[i] * 1) && !isNaN(string[i + 1] * 1)) {
-            //     let nr = string[i] + string[i + 1];
-            //     for (let a = i + 2; a < i + 5 && a < string.length; a++) {
-            //         arrLetter.push(string[a]);
-            //     }
-            //     i++; // Increment i by 1 more to skip the next digit
-            // }
         }
     }
 }
 
-alpha("1blueqw");
+alpha("blue;1green");
 
