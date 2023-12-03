64eb2710a9af10e7729326f4707c80b21aeb44fe
Victore <stanescuv19@gmail.com.com>
2023-12-03 22:02:31 +0100

    still working..

--- 2/index.js
+++ 2/index.js
@@ -9,67 +9,82 @@ const pSplit = p.split("Game")
 console.log(pSplit)
 
 // make it cleaner 
-//ex : "8:3green,10red,15blue;1green,9red;9blue,2green,12red"
 // goal : {GameId : 8 , red: [10,9,12], blue:[15,9]...}
+//ex : "8:3green,10red,15blue;1green,9red;9blue,2green,12red"
+let goal = {};
 
+alpha("8red,2green");
 function alpha(string) {
+    //input : 8red,2green,3red,4blue
+    //output: 8 red 2 green 3 red 4 blue 
+
     let gameId = string[0];
     let arrLetter = [];
-    for (let i = 0; i < string.length; i++) {
-        if (!isNaN(string[i] * 1)) {
-       
-       
-            // IF 2 DIGIT NUMBER
-        if (!isNaN(string[i] * 1) && !isNaN(string[i + 1] * 1)) {
-            let nr = string[i] + string[i + 1];
-            for (let a = i + 2; a < i + 7 && a < string.length; a++) {
-                arrLetter.push(string[a]);
-                let word = arrLetter.join("");
-                if (colors.includes(word)) {
-                    console.log("Match:", word, nr);
-                    return;
-                }
-                if (colors.includes(word.slice(0, -1))) {
-                    console.log("Match without last character:", word, nr);
-                    return;
-                }
-                if (colors.includes(word.slice(0, -2))) {
-                    console.log("Match without last two characters:", word, nr);
-                    return;
-                }
-            }
-        }
+    for (let i = 0; i <= string.length; i++) {
+        
+        // If current character is a digit
+        if (!isNaN(parseInt(string[i]))) {
 
-
-            // If current character is a digit
 
             // IF 1 DIGIT NUMBER
-            let nr = string[i];
-            for (let a = i + 1; a < i + 6 && a < string.length; a++) {
+            let nr = parseInt(string[i]);
+
+            //loop to obtain the word green/blue/red
+            for (let a = i + 1; a <= i + 6 && a <= string.length; a++) {
+                //all the letters are pushed into an array
                 arrLetter.push(string[a]);
+                // all the letters are joined into a word
                 let word = arrLetter.join("");
                 if( colors.includes(word) /*green*/){
                     console.log(word)
                     console.log(nr)
-                    return
+                   
                 }
 
                 if( colors.includes(word.slice(0,-1)) /*blue*/){
                     console.log(word)
                     console.log(nr)
-                    return
+                    
                 }
 
                 if( colors.includes(word.slice(0,-2)) /*red*/){
                     console.log(word)
                     console.log(nr)
-                    return 
+                    
                 }
             }
 
+
+
+
+
+
+       
+            // IF 2 DIGIT NUMBER
+        // if (!isNaN(string[i] * 1) && !isNaN(string[i + 1] * 1)) {
+        //     let nr = string[i] + string[i + 1];
+        //     for (let a = i + 2; a < i + 7 && a < string.length; a++) {
+        //         arrLetter.push(string[a]);
+        //         let word = arrLetter.join("");
+        //         if (colors.includes(word)) {
+        //             console.log("Match:", word, nr);
+        //             return;
+        //         }
+        //         if (colors.includes(word.slice(0, -1))) {
+        //             console.log("Match without last character:", word, nr);
+        //             return;
+        //         }
+        //         if (colors.includes(word.slice(0, -2))) {
+        //             console.log("Match without last two characters:", word, nr);
+        //             return;
+        //         }
+        //     }
+        // }
+
+
+          
         }
     }
 }
 
-alpha("green1blue");
 
